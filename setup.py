257eab8d272b835366f369d2b0fa2650494194ca
rush_wsl <rsheta@gopgf.com>
2025-07-11 20:50:44 +0000

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='google_api_wrapper',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=requirements,
     author='Rushank Sheta',
